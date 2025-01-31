50c6c00880c3ede4378bd85a496d6db1c145bbf3
Yura Shaplavsky <yurikorock777@gmail.com>
2025-01-31 17:57:36 +0200

    add gallery murkup

--- js/gallery.js
+++ js/gallery.js
@@ -63,3 +63,23 @@ const images = [
     description: "Lighthouse Coast Sea",
   },
 ];
+const gallery = document.querySelector(".gallery");
+
+// console.log(gallery);
+
+const galleryMarkup = images
+  .map(
+    ({ preview, original, description }) => `<li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
